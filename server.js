a8570a2416dd589deb044e251450dc87eaee45bd
Anurag-1205 <anuragkaushal101@gmail.com>
2025-02-10 10:46:47 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require("path")
+const port = 3000;
+const app = express();
+const cors = require("cors");
+
+app.use(bodyParser.json());
+app.use(cors());
+
+   
+app.post("/todos", (req, res) => {
+  let newTodo = {
+    id: Math.floor(Math.random() * 100000),
+    title: req.body.title,
+    description: req.body.description
+  };
+  fs.readFile("todos.json", "utf8", (err, data) => {
+    if(err) throw err;
+    const todos = JSON.parse(data);
+    todos.push(newTodo);
+    fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
+      if(err)throw err;
+      res.status(201).json(newTodo);
+    });
+  });
+});
+
+app.get("/todos", (req, res) => {
+  fs.readFile("todos.json", "utf8", (err, data) => {
+    if(err) throw err;
+    res.json(JSON.parse(data));
+  })
+})
+
+app.get("/todos/:id", (req, res) => {
+  fs.readFile("todos.json", "utf8", (err, data) => {
+    if(err) throw err;
+    const todos = JSON.parse(data);
+    const todo = todos.find(t => t.id === parseInt(req.params.id));
+    if(!todo){
+      res.status(404).send();
+    } else{
+      res.json(todo);
+    }
+  });
+});
+
+app.put("/todos/:id", (req, res) => {
+  fs.readFile("todos.json", "utf8", (err, data) => {
+    if(err) throw err;
+    const todos = JSON.parse(data);
+    const todoIndex = todos.findIndex(t => t.id === parseInt(req.params.id))
+    if(todoIndex === -1){
+      res.status(404).send();
+    }else{
+      todos[todoIndex].title = req.body.title;
+      todos[todoIndex].description = req.body.description;
+      fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
+        if(err) throw err;
+      })
+      res.json(todos[todoIndex]);
+    }
+  });
+});
+
+app.delete("/todos/:id", (req, res) => {
+  fs.readFile("todos.json", "utf8", (err, data) => {
+    if(err) throw err;
+    const todos = JSON.parse(data);
+    const todoIndex = todos.findIndex(t => t.id === parseInt(req.params.id))
+  if(todoIndex === -1){
+    res.status(404).send()
+  }else{
+    todos.splice(todoIndex, 1);
+    fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
+      if(err) throw err;
+    })
+    res.status(200).send()
+  }
+  })
+  
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+})
+
+function startServer(){
+  console.log(`Server in running on port: ${port}`);
+}
+app.listen(port, startServer);
